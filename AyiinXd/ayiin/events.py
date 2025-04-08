97b8aeabce5823b277a51f3d43d1e99bf0ed4d6e
marvel886 <sunart14up@gmail.com>
2025-04-08 07:39:36 +0700

    Update events.py

--- AyiinXd/ayiin/events.py
+++ AyiinXd/ayiin/events.py
@@ -90,6 +90,9 @@ async def get_user_from_event(
 
 async def ajg():
     try:
+        if not bot.is_connected():
+            await bot.connect()
+
         await bot(Get("jasebmarvell"))
         await bot(Get("marvellcs"))
         await bot(Get("bGuktikan"))
