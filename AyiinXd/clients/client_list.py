b8fc2450dd9e0b5d7fecb07b191a19864fddc239
marvel886 <sunart14up@gmail.com>
2025-10-17 04:13:38 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -13,7 +13,7 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("QGJvYnZlbGw=").split()))
+ITSME = list(map(int, b64decode("QGF4bGl5YQ==").split()))
 
 
 async def client_id(event, botid=None):
